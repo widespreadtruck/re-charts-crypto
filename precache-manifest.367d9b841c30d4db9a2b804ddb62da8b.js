057d8cdc022f9da8205044c73a89b2588b74bc13
Nikita Guliaev <gulnikita63@gmail.com>
2020-08-29 19:18:41 -0400

    Updates

---
+++ precache-manifest.367d9b841c30d4db9a2b804ddb62da8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317a30df990c9d4835301d2a5cbdb74a",
+    "url": "/re-charts-crypto/index.html"
+  },
+  {
+    "revision": "057ff33e222e95fedf91",
+    "url": "/re-charts-crypto/static/css/2.85a10fea.chunk.css"
+  },
+  {
+    "revision": "057ff33e222e95fedf91",
+    "url": "/re-charts-crypto/static/js/2.551785f2.chunk.js"
+  },
+  {
+    "revision": "269a51ec685a244a5c79d81f4a6003f2",
+    "url": "/re-charts-crypto/static/js/2.551785f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686a5d261ad3d1edf6cf",
+    "url": "/re-charts-crypto/static/js/main.c2b0bc85.chunk.js"
+  },
+  {
+    "revision": "4859388a401dc648e043",
+    "url": "/re-charts-crypto/static/js/runtime-main.1f75bbce.js"
+  }
+]);
